99ee3051319cc3fc89410fa0a183d044c05f849e
adinda.zafira <adindazafira19@gmail.com>
2022-03-15 19:17:40 +0700

    first commit

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,9 @@
 require('dotenv').config()
 module.exports = {
   "development": {
-    "username": process.env.DB_USERNAME_DEV || "postgres",
-    "password": process.env.DB_PASSWORD_DEV || "postgres",
-    "database": process.env.DB_DEV || "c8-dev",
+    "username": process.env.DB_USERNAME_DEV,
+    "password": process.env.DB_PASSWORD_DEV,
+    "database": process.env.DB_DEV,
     "host": "127.0.0.1",
     "dialect": "postgres"
   },
